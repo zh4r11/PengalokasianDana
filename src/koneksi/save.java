9863c9b04cca65d2361fa245cfd29353e0971d98
zh4r11 <49138265+zh4r11@users.noreply.github.com>
2023-11-18 12:17:17 +0700

    another add

---
+++ src/koneksi/save.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+/**
+ *
+ * @author Azhari
+ */
+public class save {
+    private static String ktp;
+    
+    public static String getKTP(){
+        return ktp;
+    }
+    
+    public void setKTP(String noKtp){
+        save.ktp = noKtp;
+    }
+    
+    
+}
